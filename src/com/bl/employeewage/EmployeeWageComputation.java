d20f71b71db7702863c60b668f99561a5e03c5fe
RohitGupta8 <therohitg@gmail.com>
2021-09-21 17:29:09 +0530

    Add: Solving by switch statement

--- src/com/bl/employeewage/EmployeeWageComputation.java
+++ src/com/bl/employeewage/EmployeeWageComputation.java
@@ -4,21 +4,35 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation\n");
-        double isEmployee = Math.floor(Math.random()*10)%3;
-        int employeeHrs=0;
+        int isEmployee = (int) Math.floor(Math.random() * 10) % 3;
+        int employeeHrs = 0;
         int employeeRate = 20;
         int employeeWage;
-        if (isEmployee==0){
-            System.out.println("Employee is Absent");
-            employeeHrs = 0;
-        }else if (isEmployee==1) {
-            System.out.println("Employee is Present");
-            employeeHrs = 8;
-        }else if (isEmployee==2) {
-            System.out.println("Employee doing part time");
-            employeeHrs = 3;
+//        if (isEmployee==0){
+//            System.out.println("Employee is Absent");
+//            employeeHrs = 0;
+//        }else if (isEmployee==1) {
+//            System.out.println("Employee is Present");
+//            employeeHrs = 8;
+//        }else if (isEmployee==2) {
+//            System.out.println("Employee doing part time");
+//            employeeHrs = 3;
+//        }
+        switch (isEmployee) {
+            case 0 -> {
+                System.out.println("Employee is Absent");
+                employeeHrs = 0;
+            }
+            case 1 -> {
+                System.out.println("Employee is Present");
+                employeeHrs = 8;
+            }
+            case 2 -> {
+                System.out.println("Employee doing Part Time");
+                employeeHrs = 3;
+            }
         }
         employeeWage = employeeHrs * employeeRate;
-        System.out.println("\nEmployee Wage is Rs."+ employeeWage );
+        System.out.println("\nEmployee Wage is Rs." + employeeWage);
     }
 }
