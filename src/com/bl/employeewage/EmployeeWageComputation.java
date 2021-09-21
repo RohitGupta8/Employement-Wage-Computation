6c16316e756337c473cb6d73b9ad19b43fa5008f
RohitGupta8 <therohitg@gmail.com>
2021-09-21 17:05:11 +0530

    Welcome Message

---
+++ src/com/bl/employeewage/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.bl.employeewage;
+
+public class EmployeeWageComputation {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
