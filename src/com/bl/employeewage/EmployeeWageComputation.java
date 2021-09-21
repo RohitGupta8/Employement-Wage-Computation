973a5a64e282c19b7267e843c6269ef04162c645
RohitGupta8 <therohitg@gmail.com>
2021-09-21 17:09:33 +0530

    Add: Check Employee Attendence

--- src/com/bl/employeewage/EmployeeWageComputation.java
+++ src/com/bl/employeewage/EmployeeWageComputation.java
@@ -4,5 +4,11 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+        double isEmployee = Math.floor(Math.random()*10)%2;
+        if (isEmployee==0){
+            System.out.println("Employee is Absent");
+        }else {
+            System.out.println("Employee is Present");
+        }
     }
 }
