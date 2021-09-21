47c18050ebeb57906cdaadd8a26b09ad5b8ee1b7
RohitGupta8 <therohitg@gmail.com>
2021-09-21 17:16:45 +0530

    Add: Calculate Employee Wage

--- src/com/bl/employeewage/EmployeeWageComputation.java
+++ src/com/bl/employeewage/EmployeeWageComputation.java
@@ -3,12 +3,19 @@ package com.bl.employeewage;
 public class EmployeeWageComputation {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
+        System.out.println("Welcome to Employee Wage Computation\n");
         double isEmployee = Math.floor(Math.random()*10)%2;
+        int employeeHrs;
+        int employeeRate = 20;
+        int employeeWage;
         if (isEmployee==0){
             System.out.println("Employee is Absent");
+            employeeHrs = 0;
         }else {
             System.out.println("Employee is Present");
+            employeeHrs = 8;
         }
+        employeeWage = employeeHrs * employeeRate;
+        System.out.println("\nEmployee Wage is Rs."+ employeeWage );
     }
 }
