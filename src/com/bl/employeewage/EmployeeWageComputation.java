d861d165581c3627f7241466e32a17682bf31525
RohitGupta8 <therohitg@gmail.com>
2021-09-22 09:40:55 +0530

    ADD: Calculte Employee Wage of multi companies

--- src/com/bl/employeewage/EmployeeWageComputation.java
+++ src/com/bl/employeewage/EmployeeWageComputation.java
@@ -3,37 +3,37 @@ package com.bl.employeewage;
 public class EmployeeWageComputation {
     int employeeHrs, totalEmployeeWage = 0, totalEmployeeHrs = 0, totalWorkDay = 0;
 
-    public  void computeEmployeeWage (String company,int employeeRate,int number_of_working_day,int maxHrs) {
-        while ( totalWorkDay < number_of_working_day) {
-            int isEmployee = (int) Math.floor ( Math.random () * 10 ) % 3;
+    public void computeEmployeeWage(String company, int employeeRate, int number_of_working_day, int maxHrs) {
+        while (totalWorkDay < number_of_working_day) {
+            int isEmployee = (int) Math.floor(Math.random() * 10) % 3;
             totalWorkDay++;
             switch (isEmployee) {
                 case 0 -> {
-                    System.out.println ( "Employee is Absent" );
+                    System.out.println("Employee is Absent");
                     employeeHrs = 0;
                 }
                 case 1 -> {
-                    System.out.println ( "Employee is Present" );
+                    System.out.println("Employee is Present");
                     employeeHrs = 8;
                 }
                 default -> {
-                    System.out.println ( "Employee doing Part Time" );
+                    System.out.println("Employee doing Part Time");
                     employeeHrs = 3;
                 }
             }
             totalEmployeeHrs = totalEmployeeHrs + employeeHrs;
-            System.out.println ( "Day " + totalWorkDay + " Employee Hours: " + employeeHrs );
+            System.out.println("Day " + totalWorkDay + " Employee Hours: " + employeeHrs);
         }
         totalEmployeeWage = totalEmployeeHrs * employeeRate;
-        System.out.println ( "Total Working Hours: " + totalEmployeeHrs );
-        System.out.println ( "Total Employee wage of "+company+" for "+number_of_working_day+" days  Rs. " + totalEmployeeWage );
+        System.out.println("Total Working Hours: " + totalEmployeeHrs);
+        System.out.println("Total Employee wage of " + company + " for " + number_of_working_day + " days  Rs. " + totalEmployeeWage);
 
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation\n");
-        EmployeeWageComputation employeeWageComputation=new EmployeeWageComputation();
-       employeeWageComputation. computeEmployeeWage("Reliance",200,30,9);
-        employeeWageComputation.computeEmployeeWage("DMart",180,30,10);
+        EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation();
+        employeeWageComputation.computeEmployeeWage("Reliance", 200, 30, 9);
+        employeeWageComputation.computeEmployeeWage("DMart", 180, 30, 10);
     }
 }
